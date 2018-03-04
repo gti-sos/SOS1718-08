133147ccf6ea19f648630c062e9fc68cfba9d66e
sos171808jepm <josebhpy@gmail.com>
2018-03-04 17:08:17 +0000

    módulo creado con package, index, .ignore del node_modules y express instalado

---
+++ index.js
@@ -0,0 +1,18 @@
+//Para usar en nuestro módulo uno instalado:
+//var modulo = require("nombredelmoduloinstalado");
+
+
+console.log("Hola mundo");
+
+
+
+var express = require("express");
+var app = express();
+
+app.get("/hello",(req,res)=>{
+
+    res.send("hello!");        
+});
+
+//Aquí hay que sustituir port por el puerto a usar, cloud9 bloquea algunos
+app.listen(process.env.PORT);
