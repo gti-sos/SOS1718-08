b484202f2a5c7dbfb501a48cbbc6de9af6703460
sos171808jepm <josebhpy@gmail.com>
2018-03-11 13:22:55 +0000

    configurado el archivo index.js para que lance el recurso / y use index.html de public

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-//Para usar en nuestro módulo uno instalado:
-//var modulo = require("nombredelmoduloinstalado");
-
-
-console.log("Hola mundo");
-
-
-
 var express = require("express");
+
+//Va al proceso, pilla las variables de entorno del sistema y devuelve el valor de la PORT
+var port = (process.env.PORT || 1607);
 var app = express();
 
-app.get("/hello",(req,res)=>{
 
-    res.send("Hello World!");        
-});
 
-//Aquí hay que sustituir port por el puerto a usar, cloud9 bloquea algunos
-app.listen(process.env.PORT);
+//########################################################//
+var BASE_API_URL = "/api/v1";
+//########################################################//
+
+
+
+//Esto es lo último
+//app.use("/", express.static("/home/ubuntu/workspace/API 03/public"));
+//Para que pille la dirección referente al directorio donde se está ejecutando, por si lo movemos de directorio no tener que andar cambiando la url
+app.use("/", express.static(__dirname + "/public"));
+
+
+
+//app.listen(port);
+app.listen(port,()=>{
+    console.log("esto se verá cuando ya esté listo el servidor");
+})
+//un ejemplo de sistema de gestión de eventos
+.on("error",(e)=>{console.log("Server not ready"+e)})
+;
+
+console.log("esto salrá al ejecutar el index");
+
