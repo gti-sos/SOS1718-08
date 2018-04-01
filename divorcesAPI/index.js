ffa782395976ca18f26ada4ff3acd6680b430162
sos171803jmja <jurado910@hotmail.com>
2018-04-01 21:16:01 +0000

    index

--- divorcesAPI/index.js
+++ divorcesAPI/index.js
@@ -20,8 +20,8 @@ DivorcesAPI.register = function(app,db) {
 ];
 
     console.log("Registering router for divorces API...")
-    app.get(BASE_API_PATH + "/divorces-an/help", (req, res) => {
-        res.redirect("https://documenter.getpostman.com/view/3897840/herokujmja/RVu1HAqF");
+    app.get(BASE_API_PATH + "/divorces-an/docs", (req, res) => {
+        res.redirect("https://documenter.getpostman.com/view/3897840/herokujmja/RVu1HAqFgi");
     });
 
 
