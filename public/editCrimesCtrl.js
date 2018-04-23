0af35c7ae375a84a086163245a2bc1158220003d
sos171808jepm <josebhpy@gmail.com>
2018-04-23 01:01:40 +0000

    añadido vista y controlador de edición crimes-an

--- public/editCrimesCtrl.js
+++ public/editCrimesCtrl.js
@@ -1,10 +1,10 @@
     /* global angular */
     angular
-        .module("CrimesManagerApp")
+        .module("ManagerApp")
         .controller("EditCtrl", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams,$location) {
             console.log("Edit Controller initialited");
     
-            var direccioncrimen = "/api/v1/crimes-an/" + $routeParams.province;
+            var direccioncrimen = "/api/v1/crimes-an/" + $routeParams.province+"/"+$routeParams.year+"/"+$routeParams.gender;
     
             //Funcion que crea un contacto nuevo al darle al boton send
            
@@ -14,10 +14,22 @@
             });
            
            
-            $scope.updateCrime = function (){
-            $http.put(direccioncrimen,$scope.updatedCrime).then(function (response){
-                $scope.status = "Status: " + response.status;
-                $location.path("/");
+            $scope.updateCrime = function(){
+            $http.put(direccioncrimen,$scope.updatedCrime).then(function successCallback(response){
+                $scope.status = response.status;
+                $location.path("/crimes-an");
+                $scope.error="";
+
+            },function errorCallback(response) {
+                console.log(response.status);
+                $scope.status = response.status;
+                switch (response.status) {
+                    case 404:
+                        $scope.error = "The table is empty. Fill it and try again";
+                        break;
+                    default:
+                        $scope.error = "Ups, something was wrong. Try it later";
+                }
             });
         };
     
