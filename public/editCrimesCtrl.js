bd5843190c16b9cba1c32af1f0bb9f11fa12ece5
sos171808jepm <josebhpy@gmail.com>
2018-04-23 07:23:56 +0000

    intento

--- public/editCrimesCtrl.js
+++ public/editCrimesCtrl.js
@@ -1,36 +1,27 @@
-/* global angular */
-
-angular
-    .module("ManagerApp")
-    .controller("EditCtrl", ["$scope","$http","$routeParams","$location", function($scope,$http,$routeParams,$location) {
-        
-        console.log("Edit Ctrl initialized!");
-        var direccioncrime = "/api/v1/crimes-an/"+$routeParams.province+"/"+$routeParams.year+"/"+$routeParams.gender;
-
-        $http.get(direccioncrime).then(function (response){
+    /* global angular */
+    angular
+        .module("CrimesManagerApp")
+        .controller("EditCtrl", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams,$location) {
+            console.log("Edit Controller initialited");
+    
+            var direccioncrimen = "/api/v1/crimes-an/" + $routeParams.province;
+    
+            //Funcion que crea un contacto nuevo al darle al boton send
+           
+           
+           $http.get(direccioncrimen).then(function (response){
                 $scope.updatedCrime = response.data[0];
-            
-        });
-        
-        $scope.updateCrime = function successCallback(){
-            $http.put(direccioncrime,$scope.updatedCrime).then(function (response){
-                $scope.status = response.status;
-                console.log($scope.status);
-                $location.path("/crimes-an");
-                $scope.error="";
-            }, function errorCallback(response){
-                console.log(response.status);
-                $scope.status = response.status;
-                switch (response.status) {
-                    case 405:
-                        $scope.error = "The put method has to be done to a specific resource";
-                        break;
-                    case 400:
-                        $scope.error = "Invalid fields";
-                        break;
-                    default:
-                        $scope.error = "Ups, something was wrong. Try it later";
-                }
             });
-        }
-    }]);
+           
+           
+            $scope.updateCrime = function (){
+            $http.put(direccioncrimen,$scope.updatedCrime).then(function (response){
+                $scope.status = "Status: " + response.status;
+                $location.path("/");
+            });
+        };
+    
+    
+    
+    
+        }]);
