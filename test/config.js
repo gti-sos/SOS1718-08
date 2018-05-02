99d9088eb9da6b15c34048ac2d8d5e5a2e5d3977
sos171808jepm <josebhpy@gmail.com>
2018-05-02 16:22:10 +0000

    añadido test para añadido de datos front-end crimes

--- test/config.js
+++ test/config.js
@@ -3,7 +3,7 @@ exports.config = {
     //dirección en la que va a estar el navegador en el que vamos a lanzar las pruebas (phantom)
     seleniumAddress: 'http://localhost:8910',
     //Cuales son los archivos con los test
-    specs: ['crimes-loadData.js'],
+    specs: ['crimes-loadData.js', 'crimes-addData.js'],
     //Indica que tipo de navegador es
     capabilities: {
         'browserName' : 'phantomjs'
---
+++ test/crimes-addData.js
@@ -0,0 +1,29 @@
+describe('Add crime', function(){
+   
+   it('should add a new crime', function(){
+      browser
+      .get('https://sos171808jepm-sandbox-sos171808jepm.c9users.io/#!/crimes-an')
+      .then(function(){
+          element
+            .all(by.repeater('a in crimes'))
+            .then(function(initialCrimes){
+                //browser.driver.sleep(2000);
+                element(by.model('newCrime.province')).sendKeys('galicia');
+                element(by.model('newCrime.year')).sendKeys(2000);
+                element(by.model('newCrime.gender')).sendKeys('male');
+                element(by.model('newCrime.onecrime')).sendKeys(0.0);
+                element(by.model('newCrime.twocrime')).sendKeys(0.1);
+                element(by.model('newCrime.threecrime')).sendKeys(0.2);
+                element(by.model('newCrime.morethreecrime')).sendKeys(0.3);
+                
+                element(by.buttonText('Add')).click().then(function(){
+                   element.all(by.repeater('a in crimes')).then(function(crimes){
+                       expect(crimes.length).toEqual(initialCrimes.length+1);
+                   }); 
+                });
+            });
+      });
+   });
+   
+    
+});
