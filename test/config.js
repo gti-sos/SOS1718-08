fe4ef1e1731064e920d66c724ae67c70ccddadcc
sos171808msd <mariasoldia@gmail.com>
2018-05-06 17:25:38 +0000

    pruebas protactor

--- test/config.js
+++ test/config.js
@@ -3,7 +3,8 @@ exports.config = {
     //dirección en la que va a estar el navegador en el que vamos a lanzar las pruebas (phantom)
     seleniumAddress: 'http://localhost:8910',
     //Cuales son los archivos con los test
-    specs: ['crimes-loadData.js', 'crimes-addData.js','divorces-loadData.js', 'divorces-addData.js', 'student-loadData.js', 'student-addData.js'],
+    //'crimes-loadData.js', 'crimes-addData.js','divorces-loadData.js', 'divorces-addData.js', 
+    specs: ['student-loadData.js', 'student-addData.js'],
     //Indica que tipo de navegador es
     capabilities: {
         'browserName' : 'phantomjs'
--- test/student-loadData.js
+++ test/student-loadData.js
@@ -2,22 +2,22 @@ var fs = require("fs");
 var path = require("path");
 
 describe('Data is loaded',function (){
-    it('should show some contacts', function (){
+    it('should show some students', function (){
         browser
-            .get('https://sos1718-labs-pafmon.c9users.io')
+            .get('https://sos171808msd-sos171808msd.c9users.io/#!/students-an')
             .then(function (){
                 element
                     .all(by.repeater('student in students'))
-                    .then(function (contacts){
+                    .then(function (students){
                         browser
                             .takeScreenshot()
                             .then(function (png){
-                                var stream = fs.createWriteStream(path.join(process.cwd(),'test','output','T01.png'));
+                                var stream = fs.createWriteStream(path.join(process.cwd(),'test','outputstudents','student-01.png'));
                                 stream.write(new Buffer(png,'base64'));
                                 stream.end();
                             });
                         
-                        expect(contacts.length).toBeGreaterThan(0);
+                        expect(students.length).toBeGreaterThan(0);
                     });
             });
         
