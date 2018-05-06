8a881fcadd83fc0524772ed7a8a2086bd6e99841
sos171808jepm <josebhpy@gmail.com>
2018-05-06 23:06:55 +0000

    añadida apikey

--- public/crimesView2Ctrl.js
+++ public/crimesView2Ctrl.js
@@ -15,6 +15,7 @@ angular
                     'packages': ['geochart'],
                     // Note: you will need to get a mapsApiKey for your project.
                     // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+                    'mapsApiKey': 'AIzaSyAfubp90XvQEvR_G9FoeFgBNluvMMAJWWA'
                 });
                 google.charts.setOnLoadCallback(drawRegionsMap);
 
