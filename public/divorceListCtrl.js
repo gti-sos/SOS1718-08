f9b2b6e694ddd86c7a9c95d104c069773b6ce053
sos171803jmja <jurado910@hotmail.com>
2018-05-12 19:56:44 +0000

    arreglos

--- public/divorceListCtrl.js
+++ public/divorceListCtrl.js
@@ -9,7 +9,7 @@ angular.module("ManagerApp").controller("divorceListCtrl", ["$scope", "$http", f
 
         $scope.addDivorce = function() {
             $http.post(api, $scope.newDivorce).then(function successCallback(response) {
-                $scope.status = "Status: " + response.status;
+              //  $scope.status = "Status: " + response.status;
                 window.alert("Creado con exito");
                 getDivorces();
                 $scope.error = "";
@@ -18,16 +18,22 @@ angular.module("ManagerApp").controller("divorceListCtrl", ["$scope", "$http", f
                 $scope.status = "Status: " + response.status;
                 switch (response.status) {
                     case 405:
-                        $scope.error = "The post method has to be done to a set of resources";
+                        window.alert("The post method has to be done to a set of resources");
+                      //  $scope.error = "The post method has to be done to a set of resources";
                         break;
                     case 409:
-                        $scope.error = "The resource already exists";
+                         window.alert("The resource already exists");
+                       // $scope.error = "The resource already exists";
                         break;
                     case 400:
-                        $scope.error = "Invalid fields";
+                         window.alert("Invalid fields");
+
+                       // $scope.error = "Invalid fields";
+                       
                         break;
                     default:
-                        $scope.error = "Ups, something was wrong. Try it later";
+                     window.alert("Ups, something was wrong. Try it later");
+                       // $scope.error = "Ups, something was wrong. Try it later";
                 }
             });
         }
@@ -65,7 +71,8 @@ angular.module("ManagerApp").controller("divorceListCtrl", ["$scope", "$http", f
         $scope.fillTable = function() {
 
             $http.get(api + "/loadInitialData").then(function successCallback(response) {
-                $scope.status = "Status: " + response.status;
+              //  $scope.status = "Status: " + response.status;
+                window.alert("Rellenado con exito");
                 getDivorces();
                 $scope.error = ""
             }, function errorCallback(response) {
