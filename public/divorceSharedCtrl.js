ca4fdf7a762402cd9cf8392abcb8aca5000ea3a0
sos171803jmja <jurado910@hotmail.com>
2018-05-13 16:10:12 +0000

    arreglos

--- public/divorceSharedCtrl.js
+++ public/divorceSharedCtrl.js
@@ -105,7 +105,7 @@ angular
                  
                  Highcharts.chart('container', {
     chart: {
-        type: 'colum'
+        type: 'column'
     },
     title: {
         text: 'Divorces and Graduation rate'
