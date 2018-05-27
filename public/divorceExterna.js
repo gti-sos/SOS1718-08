fc26c3882741c37b1ccc48d36efcc1b63a68b74b
sos171803jmja <jurado910@hotmail.com>
2018-05-27 01:26:00 +0000

    flickr search

--- public/divorceExterna.js
+++ public/divorceExterna.js
@@ -1,7 +1,8 @@
 /* global angular */
 /*global Highcharts*/
+var apikey="f10b03e8ec48d7871cebf065e6153645"
 
-var FlickrURL="https://api.flickr.com/services/rest/?method=flickr.tags.getClusterPhotos&api_key=f10b03e8ec48d7871cebf065e6153645&tag="
+var FlickrURL="https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+apikey+"&user_id=&tags="
 var photos;
 var photo;
 var id="";
@@ -9,8 +10,20 @@ var secret_id="";
 var farm="";
 var server="";
 var urlfinal="";
+
+var id2="";
+var secret_id2="";
+var farm2="";
+var server2="";
+var urlfinal2="";
+
+
 var urlimg="";
 var imagenes=[];
+var imagen1="";
+var imagen2="";
+var imagen3="";
+var imagen4="";
 
 
 angular
@@ -30,7 +43,10 @@ angular
        
        $http.get(FlickrURL+$scope.widget+"&format=json&nojsoncallback=1").then(function(response){
            console.log("get")
+           
+               
            photos=response.data.photos.photo;
+           $scope.fotos=photos
            console.log(photos)
             for (var i = 0; i < photos.length; i++) {
                 id=photos[i].id
@@ -42,6 +58,14 @@ angular
                 urlimg=urlfinal
                 imagenes=urlfinal;
                 $scope.imagenes=imagenes;
+                
+                id2=photos[2].id
+                farm2=photos[2].farm
+                secret_id2=photos[2].secret
+                server2=photos[2].server
+                urlfinal2="http://farm"+farm2+".staticflickr.com/"+server2+"/"+id2+"_"+secret_id2+".jpg"
+                $scope.imagen2=urlfinal2
+                
                 
                
             }
