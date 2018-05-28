3ea070c7922e77da605037f84e046f5133aae20d
sos171803jmja <jurado910@hotmail.com>
2018-05-28 14:39:09 +0000

    flickr widget integration

--- public/divorceExterna.js
+++ public/divorceExterna.js
@@ -17,8 +17,9 @@ var urlimg="";
 var imagenes=[];
 var imagen1="";
 var imagen2="";
-var imagen3="";
-var imagen4="";
+
+var imagenfinal1='';
+
 
 
 angular
@@ -35,17 +36,21 @@ angular
        
        
        
-       
-       $http.get(FlickrURL+$scope.widget+"&format=json&nojsoncallback=1").then(function(response){
-           console.log("get")
+       $http.get("https://sos1718-08.herokuapp.com/api/v1/divorces-an/"+$scope.widget).then(function(responsedivorces){
+        $http.get(FlickrURL+$scope.widget+"&format=json&nojsoncallback=1").then(function(response){
+            
+            var div=[]
+var br=[]
+var nullity=[]
+var dataPlace=[]
+var divorcios;
            
-               
            photos=response.data.photos.photo;
            $scope.fotos=photos
+           
            console.log(photos)
             for (var i = 0; i < photos.length; i++) {
                 id=photos[i].id
-                console.log(id)
                 farm=photos[i].farm
                 secret_id=photos[i].secret
                 server=photos[i].server
@@ -55,18 +60,168 @@ angular
                 imagenes.push(urlimg);
                 $scope.imagenes=imagenes;
                 imagen1="http://farm"+photos[i].farm+".staticflickr.com/"+photos[i].server+"/"+photos[i].id+"_"+photos[i].secret+".jpg"
+                imagen2="http://farm"+photos[0].farm+".staticflickr.com/"+photos[0].server+"/"+photos[0].id+"_"+photos[0].secret+".jpg"
+                imagenfinal1=""+imagen2+""
                 $scope.imagen1=imagen1;
+                $scope.imagen2=imagen2;
+                
             }
             
-            console.log(imagenes)
+             divorcios=responsedivorces.data
+            console.log(divorcios)
            
+           for (var i = 0; i < responsedivorces.data.length; i++) {
+                    dataPlace.push("'"+responsedivorces.data[i].province + " " + parseInt(responsedivorces.data[i].year)+"'");
+                    div.push(responsedivorces.data[i].divorce)
+                    br.push(responsedivorces.data[i].break)
+                    nullity.push(responsedivorces.data[i].nullity)
+            }
+            console.log(div)
+           
+            //"background-image": imagenfinal1,
             
-        
             
+          zingchart.THEME="classic";
+var myConfig = 
+        {
+            "background-image": imagenfinal1,
+            "type": "bar",
+            "background-color": "#898d95 #4f4f4f",
+            "border-radius-top-left": 13,
+            "border-radius-top-right": 13,
+            "border-radius-bottom-right": 10,
+            "border-radius-bottom-left": 10,
+            "title": {
+                "text": "PRODUCTION REPORT",
+                "align": "right",
+                "background-color": "#f7f7f7 #e3e3e9",
+                "font-family": "Passion One",
+                "font-size": "24px",
+                "font-color": "#4f4f4f",
+                "border-radius-top-left": 10,
+                "border-radius-top-right": 10
+            },
+            "plot": {
+                "border-radius-top-left": 10,
+                "border-radius-top-right": 10,
+                "value-box": {
+                    "visible": true,
+                    "font-color": "#fff",
+                    "placement": "in",
+                    "font-angle": -90,
+                    "offset-y": "20px",
+                    "font-size": "10px",
+                    "font-weight": "normal"
+                }
+            },
+            "plotarea": {
+                "background-color": "#6c6e72",
+                "margin": "80px 60px 60px 80px"
+            },
+            "tooltip":{
+              "border-radius":6
+            },
+            "scale-x": {
+                "values": dataPlace
+                ,
+                "item": {
+                    "font-color": "#fff"
+                },
+                "line-color": "#fff",
+                "line-width": "1px",
+                "tick": {
+                    "visible": false
+                },
+                "guide": {
+                    "line-width": "1px",
+                    "line-color": "#fff",
+                    "line-style": "solid",
+                    "alpha": 0.2
+                },
+                "markers": [
+                    {
+                        "type": "area",
+                        "range": [
+                            3.5,
+                            5
+                        ],
+                        "background-color": "#000",
+                        "alpha": 0.3
+                    }
+                ]
+            },
+            "scale-y": {
+                "label": {
+                    "text": "UNITS",
+                    "font-color": "#fff",
+                    "offset-x": "-10px"
+                },
+                "multiplier": true,
+                "item": {
+                    "font-color": "#fff"
+                },
+                "line-color": "#fff",
+                "line-width": "1px",
+                "tick": {
+                    "visible": false
+                },
+                "guide": {
+                    "line-width": "1px",
+                    "line-color": "#fff",
+                    "line-style": "solid",
+                    "alpha": 0.2
+                }
+            },
+            "legend": {
+                "layout": "h",
+                "background-color": "none",
+                "border-width": 0,
+                "shadow": 0,
+                "y": "46px",
+                "toggle-action": "remove",
+                "item": {
+                    "font-color": "#fff"
+                },
+                "marker": {
+                    "type": "circle",
+                    "size": 6
+                }
+            },
+            "series": [
+                {
+                    "values": div,
+                    "text": "DIV",
+                    "background-color": "#38353c",
+                    "background-image": "https://cdn.pixabay.com/photo/2017/02/16/16/27/radial-2071767_960_720.jpg",
+                    "background-fit": "xy"
+                },
+                {
+                    "values": br,
+                    "text": "Break",
+                    "background-color": "#a50617",
+                    "background-image": "https://cdn.pixabay.com/photo/2017/02/16/16/27/radial-2071769_960_720.jpg",
+                    "background-fit": "xy"
+                },{
+                    "values": nullity,
+                    "text": "Nullity",
+                    "background-color": "#006BFB",
+                    "background-image": "https://cdn.pixabay.com/photo/2017/02/16/16/27/radial-2071769_960_720.jpg",
+                    "background-fit": "xy"
+                }
+            ]
+        };
+ 
+zingchart.render({ 
+	id : 'myChart', 
+	data : myConfig, 
+	height: 500, 
+	width: 725 
+});
            
            
         });
-        
+
+       });        
         
         
      
