cb41ee732367c2b7f108ec431ee1451aa55c07d1
sos171803jmja <jurado910@hotmail.com>
2018-06-02 14:56:58 +0000

    INE tabla

--- public/divorceINE.js
+++ public/divorceINE.js
@@ -4,8 +4,8 @@
 
 angular
     .module("ManagerApp")
-    .controller("divorceTwitter", ["$scope","$http",'$sce', function($scope,$http,$sce) {
-        console.log("Twitter Api Ctrl initialized!");
+    .controller("divorceINE", ["$scope","$http",'$sce', function($scope,$http,$sce) {
+        console.log("INE Api Ctrl initialized!");
         console.log("entrando")
         
         
@@ -18,14 +18,31 @@ angular
         ////////////////
         var ine
         var div
-         $http.get("http://servicios.ine.es/wstempus/js/ES/DATOS_TABLA/6529?nult=1").then(function(responseine){
+        var nombre=[]
+        var valor=[]
+        var year=[]
+         $http.get("https://servicios.ine.es/wstempus/js/ES/DATOS_TABLA/6529?nult=1").then(function(responseine){
               $http.get("/api/v1/divorces-an").then(function(responsediv){
                   
               ine=responseine.data
               div=responsediv.data
               
+              for (var i = 0; i < responseine.data.length; i++) {
+                  nombre.push(responseine.data[i].Nombre)
+                  valor.push(responseine.data[i].Data[0].Valor)
+                  year.push(responseine.data[i].Data[0].Anyo)
+                    
+                  
+                  
+              }
+              
+              $scope.nombre=nombre
+              $scope.valor=valor
+              $scope.year=year
               console.log(ine)
               console.log(div)
+              $scope.ine=ine
+              $scope.div=div
              
              
              
